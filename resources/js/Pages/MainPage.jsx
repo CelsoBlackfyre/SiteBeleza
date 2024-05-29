1a2cea34c6b4982347589ba12c7c091188a9ebb0
Celso Blackfyre <celsomartinscarvalho@gmail.com>
2024-05-29 20:22:48 -0300

    update aula

---
+++ resources/js/Pages/MainPage.jsx
@@ -0,0 +1,10 @@
+import NavBar from "@/Components/NavBar";
+
+export default function MainPage() {
+    return (
+        <>
+            <NavBar>
+            <h1>teste</h1>
+        </>
+    );
+}
---
+++ resources/views/loja/mainpage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Inicio</title>
+</head>
+
+<body>
+    <h1>teste</h1>
+</body>
+
+</html>
